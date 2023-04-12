a3dcbe8f54b703f1aeb4198e6b9fd251524a89da
Matti Räty <mattiraty@hotmail.com>
2023-04-12 15:45:24 +0300

    working on adding map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { GoogleMap, useLoadScript } from '@react-google-maps/api'
+
 
 export default function Map() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    })
+   
   return (
     <div className='map'>
         <p>map</p>
