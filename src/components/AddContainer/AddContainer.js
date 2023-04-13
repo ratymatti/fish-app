50164f620a6599a9fcbdc792066bf6230ef8bee2
Matti Räty <mattiraty@hotmail.com>
2023-04-13 11:18:43 +0300

    created divs for components and stuff

--- src/components/AddContainer/AddContainer.js
+++ src/components/AddContainer/AddContainer.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import Map from '../Map/Map';
 import Add from '../Add/Add';
+import './AddContainer.css';
 
 export default function AddContainer(props) {
     const [active, setActive] = useState('map');
 
     if (active === 'map') {
         return (
-            <Map
+            <div className='map'>
+                <Map
                 center={props.location} />
+                <button>Add location</button>
+            </div>
         )
     } else {
         return (
-            <Add
+            <div className='add'>
+                <Add
                 addFish={props.addFish} />
+            </div>
         )
     }
 }
