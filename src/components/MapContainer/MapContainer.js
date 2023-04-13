75b679b09a0a944a09726b9a8b439bd51c4232ac
Matti Räty <mattiraty@hotmail.com>
2023-04-13 11:31:53 +0300

    created MapContainer -component

---
+++ src/components/MapContainer/MapContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from '../Map/Map';
+
+export default function MapContainer(props) {
+  return (
+    <div className='map-container'>
+        <Map
+            center={props.center}
+            zoom={10} />
+    </div>
+  )
+}
+
