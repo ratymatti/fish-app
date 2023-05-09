990491c187cfb20e24aa38f3ae93bbce783f2d9e
Matti Räty <mattiraty@hotmail.com>
2023-05-09 15:12:45 +0300

    moved fetching weather to it's own module

---
+++ src/modules/fetchWeather/fetchWeather.js
@@ -0,0 +1,22 @@
+
+async function fetchWeather(location) {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+    const units = 'metric';
+
+    if (location) {
+        try {
+            const response =  await fetch(`${apiUrl}lat=${location.lat}&lon=${location.lng}&appid=${apiKey}&units=${units}`);
+            console.log('fetched from api');
+            if (response.ok) {
+              const data = await response.json();
+              return data;
+            }
+      
+          } catch(err) {
+            console.log(err.message)
+          }
+    }
+};
+
+  export default fetchWeather;
