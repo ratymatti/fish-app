7ba50acbdd583c49f35e985f45bdb11c6d7cf8c0
Matti Räty <mattiraty@hotmail.com>
2023-05-10 16:23:05 +0300

    fixed issue from adding weather trackings

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -3,12 +3,34 @@ import './Weather.css';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import SpinningIcon from '../SpinningIcon/SpinningIcon';
 import Map from '../Map/Map';
+import fetchWeather from '../../modules/fetchWeather/fetchWeather';
 
 export default function Weather(props) {
   const [current, setCurrent] = useState('weather');
   const [newWeatherLocation, setNewWeatherLocation] = useState([]);
+  const [weatherTracking, setWeatherTracking] = useState([]);
 
   const { weather, location, active } = props;
+
+  async function addToTracking() {
+    if (newWeatherLocation) {
+      const coords = {
+         lat: newWeatherLocation[0].location.lat,
+         lng: newWeatherLocation[0].location.lng
+      }
+      const newWeather = await fetchWeather(coords);
+      
+      if (newWeather) {
+        setWeatherTracking([newWeather]); 
+      }
+      
+    }  
+  }
+
+  function handleSelection() {
+    setCurrent('weather');
+    addToTracking();
+  }
 
   if (current === 'weather') {
     return (
@@ -30,7 +52,9 @@ export default function Weather(props) {
           markerLocations={newWeatherLocation}
           setNewWeatherLocation={setNewWeatherLocation}
           active={active} />
-        <button onClick={() => setCurrent('weather')}>Set Weather Tracking</button>  
+        <button onClick={() => {
+          handleSelection()
+        }}>Set Weather Tracking</button>
       </div>
     )
   }
