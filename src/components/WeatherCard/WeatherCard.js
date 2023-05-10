d482a30121030285fd329f11559dd131352c971d
Matti Räty <mattiraty@hotmail.com>
2023-05-10 13:34:06 +0300

    refactored WeatherCard -component

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -3,23 +3,23 @@ import './WeatherCard.css';
 
 export default function WeatherCard({ data }) {
 
-    const temperature = data?.main?.temp || 0;
-    const feelsLike = data?.main?.feels_like || 0;
-    const humidity = data?.main?.humidity || 0;
-    const pressure = data?.main?.pressure || 0;
-    const windSpeed = data?.wind?.speed || 0;
-    const location = data?.name || '';
-
-    const plusMinus = temperature >= 0 ? '+' : '-';
-    
+    const content = {
+      header: `Current Weather in ${data?.name || ''}`,
+      info: [
+        { text: 'Temperature: ', value: `${data?.main?.temp || 0 >= 0 ? '+' : '-'}${data?.main?.temp || 0}` },
+        { text: 'Feels Like: ', value: `${data?.main?.feels_like || 0 >= 0 ? '+' : '-'}${data?.main?.feels_like || 0}` },
+        { text: 'Humidity: ', value: `${data?.main?.humidity || 0}%` },
+        { text: 'Pressure: ', value: `${data?.main?.pressure || 0} hPa` },
+        { text: 'Wind Speed: ' , value: `${data?.wind?.speed || 0} m/s` },
+      ]
+    };
+      
   return (
     <div className='weather-card'>
-        <h3>Current Weather in {location} </h3>
-        <h5>Temperature: {plusMinus}{temperature} </h5>
-        <h5>Feels Like: {plusMinus}{feelsLike} </h5>
-        <h5>Humidity: {humidity}%</h5>
-        <h5>Pressure: {pressure} hPa </h5>
-        <h5>Wind speed: {windSpeed} m/s </h5>
+        <h3>{content.header}</h3>
+        {content.info.map(({text, value}, index) => (
+          <h5 key={index}>{text}{value}</h5>
+        ))}
     </div>
   )
 }
