197c02ba22eeff7cb0552732f0fc66fa96aed0dd
Matti Räty <mattiraty@hotmail.com>
2023-05-17 16:09:57 +0300

    refactored weather cards to render weather icons

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -13,12 +13,14 @@ export default function WeatherCard({ data, isRemovable, removeTracking }) {
         { text: 'Pressure: ', value: `${data?.main?.pressure || 0} hPa` },
         { text: 'Wind Speed: ' , value: `${data?.wind?.speed || 0} m/s` },
       ],
-      id: data.id
+      id: data.id,
+      icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png` 
     };  
       
   return (
     <div className='weather-card'>
         <h3>{content.header}</h3>
+        <div id="icon"><img id="wicon" src={content.icon} alt="Weather icon"/></div>
         {content.info.map(({text, value}, index) => (
           <h5 key={index}>{text}{value}</h5>
         ))}
