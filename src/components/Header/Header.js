e3b70b5482774e4ec3aa4bca543da287b45908bb
Matti Räty <mattiraty@hotmail.com>
2023-09-15 19:45:09 +0300

    changed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ export default function Header({ disabled, setActive }) {
           <li><button disabled={disabled} onClick={() => setActive('MapContainer')}>Map</button></li>
           <li><button onClick={() => setActive('Weather')}>Weather</button></li>
         </ul>
-        <h1>Logbook</h1>
+        <h1>Only Fishes</h1>
       </div>  
   )
 };
