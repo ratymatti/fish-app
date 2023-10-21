b3cdc66dfab902e9b6eac5e8b63293bbc82acf6f
Matti Räty <mattiraty@hotmail.com>
2023-10-21 15:35:45 +0300

    handled rendering data to fish card

--- src/components/FishCard/FishCard.js
+++ src/components/FishCard/FishCard.js
@@ -13,17 +13,50 @@ export default function FishCard(props) {
     useEffect(() => {
         async function getFishData() {
             const fish = await fishes.find(fish => fish.id === currentFishID);
-            setCardFish(fish);
+            const fishData = {
+                header: fish.species,
+                info: [
+                    { text: "Length: ", value: `${fish.cm}cm` },
+                    { text: "Catch date: ", value: fish.dateString },
+                    { text: "Catch location: ", value: fish.water }
+                ],
+                weather: [
+                    { text: "Temperature: ", value: `${fish.weather?.temp >= 0 ? '+' : ''}${fish.weather?.temp}` || "not available" },
+                    { text: "Wind direction: ", value: fish.weather?.wind_direction || "not available" },
+                    { text: 'Humidity: ', value: `${fish.weather?.humidity || "not available"}%` },
+                    { text: 'Pressure: ', value: `${fish.weather?.pressure || "not available"} hPa` },
+                ]
+            }
+            setCardFish(fishData);
         }
         getFishData();
     },[])
     
-  return (
-    <div className='fish-card'>
-        <div className='fish-card-header'>
-            <h3 className='close-card' onClick={() => closeCard()}>X</h3>
-        </div>
-    </div>
-  )
+    if (cardFish) {
+
+        return (
+            <div className='fish-card'>
+                <div className='fish-card-header'>
+                    <h3 className='close-card' onClick={() => closeCard()}>X</h3>
+                </div>
+                <div className='fish-data'>
+                    <h3>{cardFish.header}</h3>
+                {
+                    cardFish.info.map(({text, value}, index) => (
+                        <h5 key={index}>{text}{value}</h5>
+                    ))
+                }
+                <br></br>
+                {
+                    cardFish.weather.map(({text, value}, index) => (
+                        <h5 key={index}>{text}{value}</h5>
+                    ))
+                }   
+                </div>
+            </div>
+          )
+
+    }
+  
 }
 
