a82d08f9c377585370c01ecb4d3fcdb1eba00357
Matti Räty <mattiraty@hotmail.com>
2023-10-21 14:22:22 +0300

    basic css and testing rendering fish card

--- src/components/FishCard/FishCard.js
+++ src/components/FishCard/FishCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './FishCard.css';
 
 export default function FishCard() {
   return (
     <div className='fish-card'>
-
+        <h3>fish card</h3>
     </div>
   )
 }
