fb8df35ae432f3733f8e780830b9739fafff8e4b
Matti Räty <mattiraty@hotmail.com>
2023-10-21 14:59:44 +0300

    handled getting fish data for card component

--- src/components/FishCard/FishCard.js
+++ src/components/FishCard/FishCard.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FishCard.css';
 
 export default function FishCard(props) {
     const {
         closeCard,
-        currentFish,    
+        currentFishID,
+        fishes    
     } = props;
+
+    const [cardFish, setCardFish] = useState(null);
+
+    useEffect(() => {
+        async function getFishData() {
+            const fish = await fishes.find(fish => fish.id === currentFishID);
+            setCardFish(fish);
+        }
+        getFishData();
+    },[])
     
   return (
     <div className='fish-card'>
         <div className='fish-card-header'>
-            <h3 onClick={() => closeCard()}>X</h3>
+            <h3 className='close-card' onClick={() => closeCard()}>X</h3>
         </div>
     </div>
   )
