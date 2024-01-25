20f88c9a9f2b0b5b8a25f46743862a1fff63fc6d
Matti Räty <mattiraty@hotmail.com>
2024-01-25 16:17:32 +0200

    formatted file to be easier to read

--- src/modules/fetchWeather/fetchWeather.js
+++ src/modules/fetchWeather/fetchWeather.js
@@ -1,5 +1,6 @@
 import getCurrentDate from "../getCurrentDate/getCurrentDate";
 import getCurrentTime from "../getCurrentTime/getCurrentTime";
+
 /**
  * Function fetchWeather, used for fetching current weather data from 
  * OpenWeatherMap current weather api, used to track current weather situation 
@@ -13,35 +14,34 @@ async function fetchWeather(location) {
     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
     const units = 'metric';
 
-    
-    
     const currentTime = getCurrentTime();
     const currentDate = getCurrentDate();
 
     if (location) {
         try {
             const response =  await fetch(`${apiUrl}lat=${location.lat}&lon=${location.lng}&appid=${apiKey}&units=${units}`);
-            console.log(`fetched from api ${currentTime} ${currentDate}`);
+            console.log(`fetched from api ${currentTime} ${currentDate}`); // REMOVE THIS LATER
+
             if (response.ok) {
-              const data = await response.json();
-              const weather = {
-                name: data.name,
-                id: data.id,
-                icon: data.weather[0].icon,
-                temp: data.main.temp,
-                feels_like: data.main.feels_like,
-                humidity: data.main.humidity,
-                pressure: data.main.pressure,
-                wind_speed: data.wind.speed,
-                wind_direction: data.wind.deg,
-                coords: {
-                  lat: location.lat,
-                  lng: location.lng
-                },
-                time: `${currentTime} ${currentDate}`
-              }
-              return weather;
-             
+                const data = await response.json();
+    
+                const weather = {
+                    name: data.name,
+                    id: data.id,
+                    icon: data.weather[0].icon,
+                    temp: data.main.temp,
+                    feels_like: data.main.feels_like,
+                    humidity: data.main.humidity,
+                    pressure: data.main.pressure,
+                    wind_speed: data.wind.speed,
+                    wind_direction: data.wind.deg,
+                    coords: {
+                        lat: location.lat,
+                        lng: location.lng
+                    },
+                    time: `${currentTime} ${currentDate}`
+                }
+                return weather;
             }
       
           } catch(err) {
