a4b064b6a073e8d3f5bf2abd22f901e097ddbd9b
Matti Räty <mattiraty@hotmail.com>
2024-01-25 17:31:47 +0200

    formatted code

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -5,43 +5,42 @@ import { auth } from '../../config/firebase';
 import "./Auth.css";
 
 export default function Auth(props) {
-  const {
-    setIsLoggedIn,
-    isLoggedIn,
-    setActive
-  } = props;  
+    const {
+        setIsLoggedIn,
+        isLoggedIn,
+        setActive
+    } = props;
 
-  async function signInWithGoogle() {
-    try {
-        await signInWithPopup(auth, googleProvider);
-        console.log(auth?.currentUser?.email);
-        setIsLoggedIn(true);
-    } catch(err) {
-        console.error(err);
-    }  
-  }
-  
-  async function logOut() {
-    try {
-        await signOut(auth);
-        setIsLoggedIn(false);
-        setActive('');
-        console.log("signed out")
-    } catch(err) {
-        console.error(err);
-    }
-  }
-
-  return (
-    <div className='auth'>
-        {isLoggedIn ?
-            <button
-                onClick={logOut}>Sign Out</button> :
-            <button
-                onClick={signInWithGoogle}>Sign In With Google</button>
+    async function signInWithGoogle() {
+        try {
+            await signInWithPopup(auth, googleProvider);
+            console.log(auth?.currentUser?.email);
+            setIsLoggedIn(true);
+        } catch (err) {
+            console.error(err);
         }
-        
-    </div>
-  )
+    }
+
+    async function logOut() {
+        try {
+            await signOut(auth);
+            setIsLoggedIn(false);
+            setActive('');
+            console.log("signed out")
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div className='auth'>
+            {isLoggedIn ?
+                <button
+                    onClick={logOut}>Sign Out</button> :
+                <button
+                    onClick={signInWithGoogle}>Sign In With Google</button>
+            }
+        </div>
+    )
 }
 
