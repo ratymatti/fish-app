01ac4829b3b20d1f95854cb712c034028c9389bc
Matti Räty <mattiraty@hotmail.com>
2024-01-25 16:26:27 +0200

    formatted code and added comments

--- src/modules/getLocation/getLocation.js
+++ src/modules/getLocation/getLocation.js
@@ -1,28 +1,28 @@
 /**
- * Function that gets users geolocation information from browser, if allowed.
- * 
+ * Function getLocation
+ * @description Returns the users geolocation information, if available
  * @returns object that contains users geolocation information, key lat for latitude and lng for longitude
  */
 
 async function getLocation() {
     if (navigator.geolocation) {
-      try {
-        const position = await new Promise((resolve, reject) => {
-          navigator.geolocation.getCurrentPosition(resolve, reject);
-        });
+        try {
+            const position = await new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
 
-        const { latitude: lat, longitude: lng } = position.coords;
+            const { latitude: lat, longitude: lng } = position.coords;
 
-        return { lat, lng };
+            return { lat, lng };
         
-      } catch (error) {
-        console.error(error);
-        alert(error.message);
-      }
+        } catch (error) {
+            console.error(error);
+            alert(error.message);
+        }
     } else {
-      alert('Geolocation not supported in your browser');
+        alert('Geolocation not available');
     }
-};
+}
 
 export default getLocation;
 
