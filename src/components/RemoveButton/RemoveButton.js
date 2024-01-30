75b05f01f7ff96aa4d20597eefdf2241042a56cc
Matti Räty <mattiraty@hotmail.com>
2024-01-30 10:09:37 +0200

    small refactoring

--- src/components/RemoveButton/RemoveButton.js
+++ src/components/RemoveButton/RemoveButton.js
@@ -2,30 +2,35 @@ import React, { useState } from 'react';
 import './RemoveButton.css';
 import { BsTrash } from 'react-icons/bs';
 
-export default function RemoveButton({ removeTracking, content }) {
+export default function RemoveButton(props) {
+    const {
+        content,
+        removeTracking
+    } = props;
+    
     const [clicked, setClicked] = useState(false);
 
     function handleClick(id) {
-      removeTracking(id);
-      setClicked(false);  
+        removeTracking(id);
+        setClicked(false);
     }
 
-  return (
-    <div className='remove-button'>
-      <div className='trash-button'>
-        {clicked === false && <BsTrash
-                                  onClick={() => setClicked(true)} /> }
-      </div>
-        {clicked === true && (
-            <div className='remove-buttons'>
-                <p>Are you sure?</p> 
-                <button
-                  onClick={() => handleClick(content.id)}>Yes</button>
-                <button
-                  onClick={() => setClicked(false)}>No</button>
+    return (
+        <div className='remove-button'>
+            <div className='trash-button'>
+                {clicked === false && <BsTrash
+                    onClick={() => setClicked(true)} />}
             </div>
-        )}
-    </div>
-  )
+            {clicked === true && (
+                <div className='remove-buttons'>
+                    <p>Are you sure?</p>
+                    <button
+                        onClick={() => handleClick(content.id)}>Yes</button>
+                    <button
+                        onClick={() => setClicked(false)}>No</button>
+                </div>
+            )}
+        </div>
+    )
 }
 
