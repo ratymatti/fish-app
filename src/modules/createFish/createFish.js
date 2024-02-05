3cf9b0a0aaff63b21f2715a1edde61b20332626d
Matti Räty <mattiraty@hotmail.com>
2024-02-05 14:24:10 +0200

    removed unnecessary variable

--- src/modules/createFish/createFish.js
+++ src/modules/createFish/createFish.js
@@ -3,11 +3,10 @@ import getCurrentDateString from '../getCurrentDateString/getCurrentDateString';
 import fetchWeather from '../fetchWeather/fetchWeather';
 
 async function createFish(species, cm, water, catchDate, comment, fishGeolocation) {
-    const today = new Date();
 
     let weather;
 
-    if (today.getDate() === catchDate.getDate()) {
+    if (new Date() === catchDate.getDate()) {
         weather = await fetchWeather({
             lat: fishGeolocation[0].location.lat,
             lng: fishGeolocation[0].location.lng 
