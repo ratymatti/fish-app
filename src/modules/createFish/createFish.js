e8c31cf1bee34334fbb0dc4e367ec6f00ddb4fe7
Matti Räty <mattiraty@hotmail.com>
2024-02-05 14:53:27 +0200

    created comment

--- src/modules/createFish/createFish.js
+++ src/modules/createFish/createFish.js
@@ -2,10 +2,20 @@ import { v4 as uuidv4 } from 'uuid';
 import getCurrentDateString from '../getCurrentDateString/getCurrentDateString';
 import fetchWeather from '../fetchWeather/fetchWeather';
 
+/**
+ * Function name createFish
+ * @description This function creates a fish object
+ * @param {*} species 
+ * @param {*} cm 
+ * @param {*} water 
+ * @param {*} catchDate 
+ * @param {*} comment 
+ * @param {*} fishGeolocation 
+ * @returns object
+ */
+
 async function createFish(species, cm, water, catchDate, comment, fishGeolocation) {
-
     let weather;
-
     if (new Date() === catchDate.getDate()) {
         weather = await fetchWeather({
             lat: fishGeolocation[0].location.lat,
