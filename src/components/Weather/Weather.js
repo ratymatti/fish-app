369882af3de87e336d67b8cf30e31f360f7dada5
Matti Räty <mattiraty@hotmail.com>
2024-02-06 14:32:55 +0200

    styled add tracking button

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -6,124 +6,123 @@ import Map from '../Map/Map';
 import fetchWeather from '../../modules/fetchWeather/fetchWeather';
 import { db } from '../../config/firebase';
 import {
-  collection, 
-  getDocs,
-  addDoc,
-  deleteDoc,
-  doc,
-  updateDoc
+    collection,
+    getDocs,
+    addDoc,
+    deleteDoc,
+    doc,
+    updateDoc
 } from 'firebase/firestore';
 
 export default function Weather(props) {
-  const [current, setCurrent] = useState('weather');
-  const [newWeatherLocation, setNewWeatherLocation] = useState([]);
-  const [weatherTracking, setWeatherTracking] = useState([]);
+    const {
+        weather, location,
+        active,
+    } = props;
+    
+    const [current, setCurrent] = useState('weather');
+    const [newWeatherLocation, setNewWeatherLocation] = useState([]);
+    const [weatherTracking, setWeatherTracking] = useState([]);
 
-  const {
-    weather,
-    location,
-    active,
-  } = props;
+    const weatherRef = collection(db, "weather");
 
-  const weatherRef = collection(db, "weather");
-
-  async function getDocuments() {
-    try {
-      const data = await getDocs(weatherRef);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id
-      }));
-      setWeatherTracking(filteredData);
-    } catch(err) {
-      console.error(err);
+    async function getDocuments() {
+        try {
+            const data = await getDocs(weatherRef);
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+            setWeatherTracking(filteredData);
+        } catch (err) {
+            console.error(err);
+        }
     }
-  }
 
-  async function updateWeather() {
-    for (let index in weatherTracking) {
-      try {
-        const weatherDoc = doc(db, "weather", weatherTracking[index].id);
-        const newWeather = await fetchWeather(weatherTracking[index].coords);
-        await updateDoc(weatherDoc, {...newWeather});
-      } catch(err) {
-        console.error(err);
-      }
-    }
-    getDocuments();
-  }
-
-  async function addToTracking() {
-    if (newWeatherLocation.length) {
-      const coords = {
-         lat: newWeatherLocation[0].location.lat,
-         lng: newWeatherLocation[0].location.lng
-      }
-      const newWeather = await fetchWeather(coords);
-      
-      if (newWeather) {
-        await addDoc(weatherRef, newWeather);
+    async function updateWeather() {
+        for (let index in weatherTracking) {
+            try {
+                const weatherDoc = doc(db, "weather", weatherTracking[index].id);
+                const newWeather = await fetchWeather(weatherTracking[index].coords);
+                await updateDoc(weatherDoc, { ...newWeather });
+            } catch (err) {
+                console.error(err);
+            }
+        }
         getDocuments();
-        setNewWeatherLocation([]); 
-      }
-    } 
-  }
-  
-  async function removeTracking(idToRemove) {
-    const weatherDoc = doc(db, 'weather', idToRemove);
-    await deleteDoc(weatherDoc);
-    setWeatherTracking([...weatherTracking].filter(card => card.id !== idToRemove));
-  }
+    }
 
-  function handleSelection() {
-    setCurrent('weather');
-    addToTracking();
-  }
+    async function addToTracking() {
+        if (newWeatherLocation.length) {
+            const coords = {
+                lat: newWeatherLocation[0].location.lat,
+                lng: newWeatherLocation[0].location.lng
+            }
+            const newWeather = await fetchWeather(coords);
 
-  useEffect(() => {
-    getDocuments(); 
-  },[]);
-  
-  useEffect(() => {
-    const updateInterval = 3600000;
-    let intervalID = setInterval(() => {
-      updateWeather(); 
-    }, updateInterval);
-    return(() => {
-      clearInterval(intervalID);
-    })
-  },[]);
+            if (newWeather) {
+                await addDoc(weatherRef, newWeather);
+                getDocuments();
+                setNewWeatherLocation([]);
+            }
+        }
+    }
 
-  if (current === 'weather') {
-    return (
-      <div className='weather'>
-          {!weather && <SpinningIcon />}
-          {weather && <WeatherCard
-                          data={weather} />}
-          {weatherTracking && weatherTracking.map((card, index) => (
-            <WeatherCard 
-              key={card.id}
-              data={card}
-              isRemovable={true}
-              removeTracking={removeTracking} />  
-          ))}                                              
-          <button onClick={() => setCurrent('map')}>Add new</button>                
-      </div>
-    )
-  }
+    async function removeTracking(idToRemove) {
+        const weatherDoc = doc(db, 'weather', idToRemove);
+        await deleteDoc(weatherDoc);
+        setWeatherTracking([...weatherTracking].filter(card => card.id !== idToRemove));
+    }
 
-  if (current === 'map') {
-    return (
-      <div className='map'>
-        <Map
-          center={location}
-          zoom={5}
-          markerLocations={newWeatherLocation}
-          setNewWeatherLocation={setNewWeatherLocation}
-          active={active} />
-        <button onClick={() => { handleSelection() }}>Set Weather Tracking</button>
-      </div>
-    )
-  }  
+    function handleSelection() {
+        setCurrent('weather');
+        addToTracking();
+    }
+
+    useEffect(() => {
+        getDocuments();
+    }, []);
+
+    useEffect(() => {
+        const updateInterval = 3600000;
+        let intervalID = setInterval(() => {
+            updateWeather();
+        }, updateInterval);
+        return (() => {
+            clearInterval(intervalID);
+        })
+    }, []);
+
+    if (current === 'weather') {
+        return (
+            <div className='weather'>
+                {!weather && <SpinningIcon />}
+                {weather && <WeatherCard
+                    data={weather} />}
+                {weatherTracking && weatherTracking.map((card, index) => (
+                    <WeatherCard
+                        key={card.id}
+                        data={card}
+                        isRemovable={true}
+                        removeTracking={removeTracking} />
+                ))}
+                <button onClick={() => setCurrent('map')}>Add new tracking</button>
+            </div>
+        )
+    }
+
+    if (current === 'map') {
+        return (
+            <div className='map'>
+                <Map
+                    center={location}
+                    zoom={5}
+                    markerLocations={newWeatherLocation}
+                    setNewWeatherLocation={setNewWeatherLocation}
+                    active={active} />
+                <button onClick={() => { handleSelection() }}>Set Weather Tracking</button>
+            </div>
+        )
+    }
 }
 
