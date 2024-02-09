12cb08d8533b2d02f21f0ceed971453c2c49a55e
Matti Räty <mattiraty@hotmail.com>
2024-02-09 16:10:43 +0200

    created styling for active state section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,31 +17,36 @@ export default function Header(props) {
                         <li>
                             <button
                                 onClick={() => setActive('AddContainer')}
-                                className={active === 'AddContainer' ? "active" : ""}>Add Fish
+                                className={active === 'AddContainer' ? "active" : ""}>
+                                Add Fish
                             </button>
                         </li>
                         <li>
                             <button
                                 onClick={() => setActive('Log')}
-                                className={active === 'Log' ? "active" : ""}>Log
+                                className={active === 'Log' ? "active" : ""}>
+                                Log
                             </button>
                         </li>
                         <li>
                             <button
                                 onClick={() => setActive('MapContainer')}
-                                className={active === 'MapContainer' ? "active" : ""}>Map
+                                className={active === 'MapContainer' ? "active" : ""}>
+                                Map
                             </button>
                         </li>
                         <li>
                             <button
                                 onClick={() => setActive('Weather')}
-                                className={active === 'Weather' ? "active" : ""}>Weather
+                                className={active === 'Weather' ? "active" : ""}>
+                                Weather
                             </button>
                         </li>
-                        <li>{isLoggedIn && <Auth
-                            isLoggedIn={isLoggedIn}
-                            setIsLoggedIn={setIsLoggedIn}
-                            setActive={setActive} />}
+                        <li>{isLoggedIn &&
+                            <Auth
+                                isLoggedIn={isLoggedIn}
+                                setIsLoggedIn={setIsLoggedIn}
+                                setActive={setActive} />}
                         </li>
                     </ul>
                 </div>
