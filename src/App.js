c3619464a424ebb22a19280bd7acb139d9b624f2
Matti Räty <mattiraty@hotmail.com>
2024-02-14 13:30:06 +0200

    imported weather context provider

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import getLocation from './modules/getLocation/getLocation';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Freeze from './components/Freeze/Freeze';
+
 import { FishProvider } from './contexts/FishContext';
+import { WeatherProvider } from './contexts/WeatherContext';
 
 function App() {
     const [active, setActive] = useState('');
@@ -33,23 +35,25 @@ function App() {
 
     return (
         <FishProvider>
-            <div className="App">
-                {freeze && <Freeze />}
-                <Header
-                    active={active}
-                    setActive={setActive}
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn} />
-                <MainContainer
-                    active={active}
-                    location={location}
-                    changeLocation={changeLocation}
-                    getCurrentLocation={getLocation}
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                    setFreeze={setFreeze} />
-                <Footer />
-            </div>
+            <WeatherProvider>     
+                <div className="App">
+                    {freeze && <Freeze />}
+                    <Header
+                        active={active}
+                        setActive={setActive}
+                        isLoggedIn={isLoggedIn}
+                        setIsLoggedIn={setIsLoggedIn} />
+                    <MainContainer
+                        active={active}
+                        location={location}
+                        changeLocation={changeLocation}
+                        getCurrentLocation={getLocation}
+                        isLoggedIn={isLoggedIn}
+                        setIsLoggedIn={setIsLoggedIn}
+                        setFreeze={setFreeze} />
+                    <Footer />
+                </div>
+            </WeatherProvider>
         </FishProvider>
 
     )
