51e12438aa6ff0ec35b0cd5a94a1cd9601fbd0cc
Matti Räty <mattiraty@hotmail.com>
2024-02-14 13:00:15 +0200

    removed state variable fishes and related functions from app component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainContainer from './components/MainContainer/MainContainer';
-import { sortFishes } from './modules/sortFishes/sortFishes';
-import fetchWeather from './modules/fetchWeather/fetchWeather';
 import getLocation from './modules/getLocation/getLocation';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { db } from './config/firebase';
-import { collection, getDocs } from 'firebase/firestore';
 import Freeze from './components/Freeze/Freeze';
+import { FishProvider } from './contexts/FishContext';
 
 function App() {
     const [active, setActive] = useState('');
-    const [fishes, setFishes] = useState([]);
     const [location, setLocation] = useState(null);
-    const [weather, setWeather] = useState(null);
     const [currentUserLocation, setCurrentUserLocation] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [freeze, setFreeze] = useState(false);
 
-    const fishesRef = collection(db, "fishes");
-
     function changeLocation(location) {
         setLocation(location);
-    }
-
-    function sortByField(field, direction) {
-        const sortedFishes = sortFishes(field, fishes, direction);
-        setFishes(sortedFishes);
     }
 
     useEffect(() => {
@@ -43,64 +31,27 @@ function App() {
         getCoords();
     }, [currentUserLocation]);
 
-    async function getWeather() {
-        const currentWeather = await fetchWeather(currentUserLocation, "weather");
-        setWeather(currentWeather);
-    }
-
-    useEffect(() => {
-        if (currentUserLocation) {
-            getWeather();
-        }
-
-        const updateInterval = 3600000;
-        let intervalID = setInterval(() => {
-            getWeather();
-        }, updateInterval);
-        return (() => {
-            clearInterval(intervalID);
-        })
-    }, [currentUserLocation]);
-
-    async function getDocuments() {
-        try {
-            const data = await getDocs(fishesRef);
-            const filteredData = data.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id
-            }));
-            setFishes(sortFishes('date', filteredData, 'desc'));
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    useEffect(() => {
-        getDocuments();
-    }, []);
-
     return (
-        <div className="App">
-            {freeze && <Freeze />}
-            <Header
-                active={active}
-                setActive={setActive}
-                isLoggedIn={isLoggedIn}
-                setIsLoggedIn={setIsLoggedIn} />
-            <MainContainer
-                fishes={fishes}
-                active={active}
-                sortByField={sortByField}
-                location={location}
-                changeLocation={changeLocation}
-                getCurrentLocation={getLocation}
-                weather={weather}
-                isLoggedIn={isLoggedIn}
-                setIsLoggedIn={setIsLoggedIn}
-                getDocuments={getDocuments}
-                setFreeze={setFreeze} />
-            <Footer />
-        </div>
+        <FishProvider>
+            <div className="App">
+                {freeze && <Freeze />}
+                <Header
+                    active={active}
+                    setActive={setActive}
+                    isLoggedIn={isLoggedIn}
+                    setIsLoggedIn={setIsLoggedIn} />
+                <MainContainer
+                    active={active}
+                    location={location}
+                    changeLocation={changeLocation}
+                    getCurrentLocation={getLocation}
+                    isLoggedIn={isLoggedIn}
+                    setIsLoggedIn={setIsLoggedIn}
+                    setFreeze={setFreeze} />
+                <Footer />
+            </div>
+        </FishProvider>
+
     )
 }
 
