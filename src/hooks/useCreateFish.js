ee265813cf28ae880a4453759dcce6d07b5f688d
Matti Räty <mattiraty@hotmail.com>
2024-02-15 14:44:48 +0200

    created getWeather inside useEffect that fetches weather as soon it gets necessary values for it

--- src/hooks/useCreateFish.js
+++ src/hooks/useCreateFish.js
@@ -1,18 +1,35 @@
 import { useState, useEffect } from "react";
+import fetchWeather from "../modules/fetchWeather/fetchWeather";
 
 function useCreateFish() {
+    const [location, setLocation] = useState(null);
+    const [catchDate, setCatchDate] = useState();
     const [species, setSpecies] = useState(null);
     const [cm, setCm] = useState(0);
     const [water, setWater] = useState(null);
-    const [catchDate, setCatchDate] = useState();
     const [comment, setComment] = useState('');
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        if (catchDate.getDate() === new Date().getDate()) {
+            const getWeather = async () => {
+                if (location) {
+                    const response = await fetchWeather(location, 'weather');
+                    setWeather(response);
+                }
+            }
+            getWeather();    
+        }
+    }, [catchDate, location]);
 
     return {
         species, setSpecies,
         cm, setCm,
         water, setWater,
         catchDate, setCatchDate,
-        comment, setComment
+        comment, setComment,
+        location, setLocation,
+        weather
     }
 }
 
