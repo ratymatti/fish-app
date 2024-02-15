92a04e7dfc2bd0b0c660fce655b57730e8cf8230
Matti Räty <mattiraty@hotmail.com>
2024-02-15 16:58:43 +0200

    created CreateFishContext provider to handle fish submitting states

---
+++ src/contexts/CreateFishContext.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import fetchWeather from '../modules/fetchWeather/fetchWeather';
+import getCurrentDateString from '../modules/getCurrentDateString/getCurrentDateString';
+import { v4 as uuidv4 } from 'uuid';
+
+export const CreateFishContext = React.createContext();
+
+export function CreateFishProvider({ children }) {
+    const [location, setLocation] = useState(null);
+    const [catchDate, setCatchDate] = useState(null);
+    const [species, setSpecies] = useState(null);
+    const [cm, setCm] = useState(0);
+    const [water, setWater] = useState(null);
+    const [comment, setComment] = useState('');
+    const [weather, setWeather] = useState({ info: "not available" });
+
+    useEffect(() => {
+        if (catchDate && catchDate.getDate() === new Date().getDate()) {
+            const getWeather = async () => {
+                if (location) {
+                    const response = await fetchWeather(location, 'weather');
+                    setWeather(response);
+                }
+            }
+            getWeather();    
+        }
+    }, [catchDate, location]);
+
+    function createFish() {
+        return {
+            species: species,
+            cm: cm,
+            water: water,
+            comment: comment,
+            date: catchDate,
+            dateString: getCurrentDateString(),
+            id: uuidv4(),
+            weather: weather,
+            location: {
+                lat: location.lat,
+                lng: location.lng
+            }
+        }
+    }
+
+    function setStatesToDefault() {
+        setLocation(null);
+        setCatchDate(null);
+        setSpecies(null);
+        setCm(0);
+        setWater(null);
+        setComment('');
+        setWeather({ info: "not available" });
+    }
+
+    return (
+        <CreateFishContext.Provider value={{    location, setLocation,
+                                                catchDate, setCatchDate,
+                                                species, setSpecies,
+                                                cm, setCm,
+                                                water, setWater,
+                                                setComment, createFish,
+                                                setStatesToDefault }}>
+            {children}
+        </CreateFishContext.Provider>
+    );
+}
