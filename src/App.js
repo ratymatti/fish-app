30984836bb9be0459357d073c4c0bba42bfe79e4
Matti Räty <mattiraty@hotmail.com>
2024-02-23 12:13:19 +0200

    imported location context and removed unnecessary state variables

--- src/App.js
+++ src/App.js
@@ -1,60 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MainContainer from './components/MainContainer/MainContainer';
-import getLocation from './modules/getLocation/getLocation';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Freeze from './components/Freeze/Freeze';
 
 import { FishProvider } from './contexts/FishContext';
 import { WeatherProvider } from './contexts/WeatherContext';
+import { LocationProvider } from './contexts/LocationContext';
 
 function App() {
     const [active, setActive] = useState('');
-    const [location, setLocation] = useState(null);
-    const [currentUserLocation, setCurrentUserLocation] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [freeze, setFreeze] = useState(false);
 
-    function changeLocation(location) {
-        setLocation(location);
-    }
-
-    useEffect(() => {
-        async function getCoords() {
-            const coords = await getLocation();
-            setLocation(coords);
-
-            if (!currentUserLocation) {
-                setCurrentUserLocation(coords);
-            }
-        };
-
-        getCoords();
-    }, [currentUserLocation]);
 
     return (
-        <FishProvider>
-            <WeatherProvider>
-                <div className="App">
-                    {freeze && <Freeze />}
-                    <Header
-                        active={active}
-                        setActive={setActive}
-                        isLoggedIn={isLoggedIn}
-                        setIsLoggedIn={setIsLoggedIn} />
-                    <MainContainer
-                        active={active}
-                        location={location}
-                        changeLocation={changeLocation}
-                        getCurrentLocation={getLocation}
-                        isLoggedIn={isLoggedIn}
-                        setIsLoggedIn={setIsLoggedIn}
-                        setFreeze={setFreeze} />
-                    <Footer />
-                </div>
-            </WeatherProvider>
-        </FishProvider>
+        <LocationProvider>
+            <FishProvider>
+                <WeatherProvider>
+                    <div className="App">
+                        {freeze && <Freeze />}
+                        <Header
+                            active={active}
+                            setActive={setActive}
+                            isLoggedIn={isLoggedIn}
+                            setIsLoggedIn={setIsLoggedIn} />
+                        <MainContainer
+                            active={active}
+                            isLoggedIn={isLoggedIn}
+                            setIsLoggedIn={setIsLoggedIn}
+                            setFreeze={setFreeze} />
+                        <Footer />
+                    </div>
+                </WeatherProvider>
+            </FishProvider>
+        </LocationProvider>
     )
 }
 
