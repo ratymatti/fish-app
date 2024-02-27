8ff6962b1382177272ebcbda8436a34b9ce4b23b
Matti Räty <mattiraty@hotmail.com>
2024-02-27 14:45:23 +0200

    translating project to typescript

--- src/contexts/ActiveContext.tsx
+++ src/contexts/ActiveContext.tsx
@@ -2,13 +2,21 @@ import React, { useState, createContext } from 'react';
 
 export interface ActiveContextType {
     active: string;
-    setActive: (active: string) => void;
+    setActive: (active: ActiveState) => void;
+}
+
+export enum ActiveState {
+    AddFish = 'add fish',
+    Fishes = 'fishes',
+    Map = 'map',
+    Weather = 'weather',
+    Empty = ''
 }
 
 export const ActiveContext = createContext<ActiveContextType | undefined>(undefined); 
 
 export function ActiveProvider({children}: {children: React.ReactNode}) {
-    const [active, setActive] = useState<string>('');
+    const [active, setActive] = useState<ActiveState>(ActiveState.Empty);
 
     return (
         <ActiveContext.Provider value={{ active, setActive }}>
