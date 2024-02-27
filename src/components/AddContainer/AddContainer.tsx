3aaf8ddefd173e3099eca9e3c7f7ab7b7b6becec
Matti Räty <mattiraty@hotmail.com>
2024-02-27 14:40:03 +0200

    translating project to typescript

---
+++ src/components/AddContainer/AddContainer.tsx
@@ -0,0 +1,83 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import Map from '../Map/Map';
+import Add from '../Add/Add';
+import './AddContainer.css';
+import SpinningIcon from '../SpinningIcon/SpinningIcon';
+import { LocationContext, LocationContextType } from '../../contexts/LocationContext';
+
+interface FishLocation {
+    location: {
+        lat: number;
+        lng: number;
+    }
+}
+
+interface AddContainerProps {
+    setError: Dispatch<SetStateAction<string>>;
+}
+
+export enum CurrentState {
+    Map = 'map',
+    Add = 'add',
+    Loading = 'loading'
+}
+
+export default function AddContainer(props: AddContainerProps) {
+    const { setError } = props;
+
+    const [current, setCurrent] = useState<CurrentState>(CurrentState.Map);
+    const [fishGeolocation, setFishGeolocation] = useState<FishLocation[]>([]);
+    const [disabled, setDisabled] = useState<boolean>(true);
+
+    const { userLocation, setUserLocation } = React.useContext(LocationContext) as LocationContextType;
+
+    function handleClick() {
+        if (fishGeolocation.length) {
+            setUserLocation(
+                {
+                    lat: fishGeolocation[0].location.lat,
+                    lng: fishGeolocation[0].location.lng
+                }
+            );
+            }    
+            setCurrent(CurrentState.Map);
+    }
+
+
+    if (current === CurrentState.Map && userLocation) {
+        return (
+            <div className='map'>
+                <Map
+                    zoom={12}
+                    setFishGeolocation={setFishGeolocation}
+                    markerLocations={fishGeolocation}
+                    setDisabled={setDisabled} />
+                <button className='button' disabled={disabled} onClick={() => setCurrent(CurrentState.Add)}>Select location</button>
+            </div>
+        )
+    }
+
+    if (current === CurrentState.Add) {
+        return (
+            <div className='add'>
+                <button className='button' onClick={handleClick}>Edit location</button>
+                <Add
+                    setError={setError}
+                    fishGeolocation={fishGeolocation}
+                    setCurrent={setCurrent}
+                    setFishGeolocation={setFishGeolocation} />
+            </div>
+        )
+    }
+
+    if (current === CurrentState.Loading) {
+        return (
+            <div id='loading'>
+                <SpinningIcon />
+            </div>
+        )
+    }
+
+    return null;
+}
+
