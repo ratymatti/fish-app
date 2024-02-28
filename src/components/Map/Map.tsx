73e0c25c7a0294cf401f12de750d65efd1fadec5
Matti Räty <mattiraty@hotmail.com>
2024-02-28 14:00:06 +0200

    translating project to typescript

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+import './Map.css';
+import { mapStyles } from '../../modules/mapStyles/mapStyles';
+import SpinningIcon from '../SpinningIcon/SpinningIcon';
+import createCoords, { LocationObject } from '../../modules/createCoords/createCoords';
+
+import { LocationContext, LocationContextType, UserLocation } from '../../contexts/LocationContext';
+import { ActiveContext, ActiveContextType, ActiveState } from '../../contexts/ActiveContext';
+
+
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    mapTypeControl: true,
+    zoomControl: true,
+};
+
+interface MapProps {
+    markerLocations: { id: string; location: { lat: number; lng: number } }[];
+    zoom: number;
+    setFishGeolocation?: (value: LocationObject[]) => void;
+    setDisabled?: (value: boolean) => void;
+    setNewWeatherLocation?: (value: any) => void;
+    center?: UserLocation | undefined | null;
+}
+
+interface Marker {
+    id: string;
+    location: { lat: number; lng: number };
+}
+
+export default function Map(props: MapProps) {
+    const {
+        markerLocations,
+        zoom, setFishGeolocation,
+        setDisabled, setNewWeatherLocation
+    } = props;
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY!
+    });
+
+    const [markers, setMarkers] = useState<Marker[]>([]);
+
+    const { userLocation } = React.useContext(LocationContext) as LocationContextType;
+    const { active } = React.useContext(ActiveContext) as ActiveContextType;
+
+    let center: UserLocation = { lat: 67.92, lng: 26.5 };
+
+    if (userLocation) center = userLocation;
+
+    function handleClick(event: any) {
+        const selectedLocation = createCoords(event);
+
+        if (active === ActiveState.AddFish) {
+            if (setFishGeolocation) setFishGeolocation([selectedLocation]);
+            if (setDisabled) setDisabled(false);
+        } else if (active === ActiveState.Weather) {
+            if (setNewWeatherLocation) setNewWeatherLocation([selectedLocation]);
+        }
+    }
+
+    useEffect(() => {
+        setMarkers(markerLocations);
+    }, [markerLocations]);
+
+    if (!isLoaded) {
+        return (
+            <div className='loading-map'>
+                <SpinningIcon />
+            </div>
+        )
+    }
+
+    return (
+        <GoogleMap
+            zoom={zoom}
+            center={center}
+            mapContainerClassName='map-container'
+            onClick={(event) => {
+                handleClick(event);
+            }}
+            options={options}
+        >
+            {markers.map(marker => <MarkerF
+                key={marker.id.valueOf()}
+                position={
+                    {
+                        lat: marker.location?.lat,
+                        lng: marker.location?.lng
+                    }
+                } />)}
+        </GoogleMap>
+    )
+}
