7cd4b5e2c0c4334ee30195f99cd96b94bb0bcb4e
Matti Räty <mattiraty@hotmail.com>
2024-02-28 14:04:17 +0200

    translating project to typescript

--- src/hooks/useFetchWeather.tsx
+++ src/hooks/useFetchWeather.tsx
@@ -2,8 +2,46 @@ import getCurrentTime from '../modules/getCurrentTime/getCurrentTime';
 import getCurrentDateString from '../modules/getCurrentDateString/getCurrentDateString';
 import { v4 as uuidv4 } from 'uuid';
 
+export enum WeatherType {
+    WEATHER = 'weather',
+    FORECAST = 'forecast'
+}
+
+enum Time {
+    TIME = '12:00:00'
+}
+
+interface Location {
+    lat: number;
+    lng: number;
+}
+
+interface WeatherInfo {
+    icon: string;
+    time: string;
+    weather: {
+        temp: number;
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        wind_speed: number;
+        wind_direction: number;
+    };
+} // LOOK INTO THIS
+
+export interface WeatherObject {
+    type: WeatherType;
+    name: string;
+    id: string;
+    coords: Location;
+    forecastArray?: WeatherInfo[] | [];
+    currentWeather?: WeatherInfo | {};
+
+}
+
 
 export function useFetchWeather() {
+
     /**
     * Function name fetchWeather
     * @description This function fetches weather data from OpenWeatherMap API
@@ -13,10 +51,10 @@ export function useFetchWeather() {
     */
 
 
-    async function fetchWeather(location, type) {
+    async function fetchWeather(location: Location, type: WeatherType) {
         const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
         const units = 'metric';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/${type}?`; // type is either 'weather' or 'forecast'
+        const apiUrl = `https://api.openweathermap.org/data/2.5/${type}?`;
 
         const currentTimeDate = `${getCurrentDateString()} ${getCurrentTime()}`; // REMOVE THIS LATER;
 
@@ -33,13 +71,17 @@ export function useFetchWeather() {
                 }
 
             } catch (err) {
-                console.log(err.message);
+                if (err instanceof Error) {
+                    console.log(err.message);
+                } else {
+                    console.log(err);
+                }
             }
         }
     }
 
-    function createWeatherObject(data, location, type) {
-        const source = type === 'weather' ? data : data.city;
+    function createWeatherObject(data: any, location: Location, type: WeatherType) {
+        const source = type === WeatherType.WEATHER ? data : data.city;
 
         const newWeatherObj = {
             type: type,
@@ -49,17 +91,17 @@ export function useFetchWeather() {
                 lat: location.lat,
                 lng: location.lng
             },
-            forecastArray: type === 'forecast' ? createForecastArray(data) : [],
-            currentWeather: type === 'weather' ? getWeatherInfo(data, 'weather') : {}
+            forecastArray: type === WeatherType.FORECAST ? createForecastArray(data) : [],
+            currentWeather: type === WeatherType.WEATHER ? getWeatherInfo(data, WeatherType.WEATHER) : {}
         }
         return newWeatherObj;
     }
 
-    function getWeatherInfo(data, type) {
+    function getWeatherInfo(data: any, type: WeatherType): WeatherInfo {
         const currentTimeDate = `${getCurrentDateString()} ${getCurrentTime()}`
         return {
             icon: data.weather[0].icon,
-            time: type === 'weather' ? currentTimeDate : data.dt_txt,
+            time: type === WeatherType.WEATHER ? currentTimeDate : data.dt_txt,
             weather: {
                 temp: data.main.temp,
                 feels_like: data.main.feels_like,
@@ -71,12 +113,12 @@ export function useFetchWeather() {
         }
     }
 
-    function createForecastArray(data) {
-        const forecastArray = [];
+    function createForecastArray(data: any): WeatherInfo[] {
+        const forecastArray: WeatherInfo[] = [];
         for (const index in data.list) {
             const time = data.list[index].dt_txt.split(' ')[1]
-            if (time === "12:00:00") {
-                const weatherObj = getWeatherInfo(data.list[index], 'forecast');
+            if (time === Time.TIME) {
+                const weatherObj = getWeatherInfo(data.list[index], WeatherType.FORECAST);
                 forecastArray.push(weatherObj);
             }
         }
