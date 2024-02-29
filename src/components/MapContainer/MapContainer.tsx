4743824984530861cb767e934191a5a558834bd0
Matti Räty <mattiraty@hotmail.com>
2024-02-29 13:52:07 +0200

    imported types and created helper function for the component

--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -3,25 +3,25 @@ import Map from '../Map/Map';
 import { FishContext, FishContextType } from '../../contexts/FishContext';
 import { LocationContext, LocationContextType } from '../../contexts/LocationContext';
 import { FishType } from '../../contexts/CreateFishContext';
-import { Location } from '../../types/location';
+import { Location, LocationObject } from '../../types/location';
+
+const filterAndTransformFishes = (fishes: FishType[]): LocationObject[] => {
+    return fishes
+        .filter(fish => fish.id && fish.location)
+        .map(fish => ({
+            id: fish.id as string,
+            location: {
+                lat: (fish.location as Location).lat,
+                lng: (fish.location as Location).lng
+            }
+        }));
+}
 
 export default function MapContainer(): JSX.Element {
     const { fishes } = React.useContext(FishContext) as FishContextType;
     const { userLocation } = React.useContext(LocationContext) as LocationContextType;
 
-    const fishArray: FishType[] = fishes;
-    const validFishArray = fishArray
-        .filter(fish => fish.id !== null && fish.id !== undefined)
-        .filter(fish => fish.location !== null && fish.location !== undefined)
-        .map(fish => {
-            return {
-                id: fish.id as string,
-                location: {
-                    lat: (fish.location as Location).lat,
-                    lng: (fish.location as Location).lng
-                }
-            }
-        });
+    const validFishArray: LocationObject[] = filterAndTransformFishes(fishes);
 
     return (
         <div className='map-container'>
