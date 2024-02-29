34c4dc476dd6747030b597f56d769f5b8962ddcb
Matti Räty <mattiraty@hotmail.com>
2024-02-29 14:00:45 +0200

    added comment

--- src/types/location.ts
+++ src/types/location.ts
@@ -1,7 +1,14 @@
 /**
  * Interface Location
  * @description Interface for the location object
- * Used in LocationContext.tsx, Map.tsx, MapContainer.tsx
+ * Used in:
+ *      LocationContext.tsx,
+ *      Map.tsx,
+ *      MapContainer.tsx,
+ *      AddContainer.tsx,
+ *      CreateFishContext.tsx,
+ *      WeatherContext.tsx
+ * 
  * !!! DON'T CHANGE THIS !!!
  */
 
@@ -15,6 +22,7 @@ export interface Location {
  * @description Interface for the position object returned by the geolocation API
  * @property coords - object that contains the latitude and longitude of the user
  * Used only in LocationContext.tsx getLocation function
+ * 
  * !!! DON'T CHANGE THIS !!!
  */
 
@@ -24,6 +32,21 @@ export interface Position {
         longitude: number;
     };
 }
+
+/**
+ * Interface LocationObject
+ * @description Interface for the location object
+ * @property location - object that contains the latitude and longitude of the location
+ * @property id - string that contains the id of the location
+ * Used in:
+ *      AddContainer.tsx,
+ *      Map.tsx,
+ *      WeatherContext.tsx,
+ *      createCoords.ts,
+ *      MapContainer.tsx
+ *  
+ * !!! DON'T CHANGE THIS !!!
+ */
 
 export interface LocationObject {
     location: Location,
