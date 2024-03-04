7b9d5d590441f365da4bcc1c011d97cfd96e0810
Matti Räty <mattiraty@hotmail.com>
2024-03-04 15:59:09 +0200

    created file that stores fish-related interfaces

---
+++ src/types/fish.ts
@@ -0,0 +1,26 @@
+import { Timestamp } from "firebase/firestore";
+import { WeatherObject } from "./weather";
+import { Location } from "./location";
+
+/**
+ * Interface FishObject
+ * @description Interface for the fish objects stored in the database
+ * Used in:
+ *      FishContext.tsx,
+ *      CreateFishContext.tsx,
+ *      sortFishes.ts,
+ *      FishCard.tsx,
+ *      MapContainer.tsx
+ */
+
+export interface FishObject {
+    id: string | null;
+    species: string | null;
+    cm: number | null;
+    date: Date | Timestamp | null;
+    dateString: string | null;
+    locationName: string | null;
+    comment: string | null;
+    weather: WeatherObject | null;
+    geolocation: Location | null;
+}
