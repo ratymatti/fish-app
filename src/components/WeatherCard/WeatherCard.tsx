e12c8769ae02907ec467a27c5b369ef814e2449d
Matti Räty <mattiraty@hotmail.com>
2024-03-04 15:52:50 +0200

    added typing to component and created state variable that stores the data that component renders

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,45 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './WeatherCard.css';
 import RemoveButton from '../RemoveButton/RemoveButton';
+import { WeatherObject } from '../../types/weather';
 
-export default function WeatherCard(props) {
+interface WeatherCardProps {
+    data: WeatherObject;
+    isRemovable: boolean;
+    removeTracking?: (id: string) => void | undefined;
+}
+
+interface CardInfo {
+    header: string;
+    info: { text: string; value: string }[];
+    id: string;
+    icon: string;
+}
+
+export default function WeatherCard(props: WeatherCardProps): JSX.Element {
     const {
         data,
         isRemovable,
         removeTracking
     } = props;
 
-    const source = data?.currentWeather?.weather;
+    const [cardInfo, setCardInfo] = useState<CardInfo>();
 
-    const content = {
-        header: `Current Weather in ${data?.name || ''}`,
-        info: [
-            { text: 'Temperature: ', value: `${source.temp >= 0 ? '+' : ''}${source.temp || 0}` },
-            { text: 'Feels Like: ', value: `${source.feels_like >= 0 ? '+' : ''}${source.feels_like || 0}` },
-            { text: 'Humidity: ', value: `${source.humidity || 0}%` },
-            { text: 'Pressure: ', value: `${source.pressure || 0} hPa` },
-            { text: 'Wind Speed: ', value: `${source.wind_speed || 0} m/s` },
-            { text: 'updated at ', value: data.currentWeather.time }
-        ],
-        id: data.id,
-        icon: `http://openweathermap.org/img/w/${data.currentWeather.icon}.png`
-    };
+
+    useEffect(() => {
+        if (data && data.currentWeather && 'weather' in data.currentWeather) {
+            const source = data?.currentWeather?.weather;
+
+            const content = {
+                header: `Current Weather in ${data?.name || ''}`,
+                info: [
+                    { text: 'Temperature: ', value: `${source.temp >= 0 ? '+' : ''}${source.temp || 0}` },
+                    { text: 'Feels Like: ', value: `${source.feels_like >= 0 ? '+' : ''}${source.feels_like || 0}` },
+                    { text: 'Humidity: ', value: `${source.humidity || 0}%` },
+                    { text: 'Pressure: ', value: `${source.pressure || 0} hPa` },
+                    { text: 'Wind Speed: ', value: `${source.wind_speed || 0} m/s` },
+                    { text: 'updated at ', value: data.currentWeather.time }
+                ],
+                id: data.id,
+                icon: `http://openweathermap.org/img/w/${data.currentWeather.icon}.png`
+            };
+            setCardInfo(content);
+        }
+    }, [data]);
+
+    if (!cardInfo) {
+        return <div>loading...</div>
+    }
 
     return (
         <div className='weather-card'>
-            <h3>{content.header}</h3>
+            <h3>{cardInfo.header}</h3>
             <div id="icon">
                 <img
                     id="wicon"
-                    src={content.icon}
+                    src={cardInfo.icon}
                     alt="Weather icon" />
             </div>
-            {content.info.map(({ text, value }, index) => (
+            {cardInfo.info.map(({ text, value }, index) => (
                 <h5 key={index}>{text}{value}</h5>
             ))}
             {isRemovable && <RemoveButton
-                removeTracking={removeTracking}
-                content={content} />}
+                removeTracking={removeTracking!}
+                content={cardInfo} />}
         </div>
     )
 }
