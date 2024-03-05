1b3ae892d08ad2f8c2cb8941316bff251a3e4db2
Matti Räty <mattiraty@hotmail.com>
2024-03-05 11:10:14 +0200

    moved createCardData function outside of the component

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -16,41 +16,42 @@ interface CardInfo {
     icon: string;
 }
 
+function createCardData(data: WeatherObject): CardInfo | null {
+    if (data && data.currentWeather && 'weather' in data.currentWeather) {
+
+        const source = data.currentWeather.weather;
+
+        return {
+            header: `Current Weather in ${data.name}`,
+            info: [
+                { text: 'Temperature: ', value: `${source.temp >= 0 ? '+' : ''}${source.temp || 0}` },
+                { text: 'Feels Like: ', value: `${source.feels_like >= 0 ? '+' : ''}${source.feels_like || 0}` },
+                { text: 'Humidity: ', value: `${source.humidity || 0}%` },
+                { text: 'Pressure: ', value: `${source.pressure || 0} hPa` },
+                { text: 'Wind Speed: ', value: `${source.wind_speed || 0} m/s` },
+                { text: 'updated at ', value: data.currentWeather.time }
+            ],
+            id: data.id,
+            icon: `http://openweathermap.org/img/w/${data.currentWeather.icon}.png`
+        }
+    }
+    return null;
+}
+
 export default function WeatherCard(props: WeatherCardProps): JSX.Element {
-    const {
-        data,
-        isRemovable,
-        removeTracking
-    } = props;
+    const { data, isRemovable, removeTracking } = props;
 
     const [cardInfo, setCardInfo] = useState<CardInfo>();
 
-
+    
     useEffect(() => {
-        if (data && data.currentWeather && 'weather' in data.currentWeather) {
-            const source = data?.currentWeather?.weather;
-
-            const content = {
-                header: `Current Weather in ${data?.name || ''}`,
-                info: [
-                    { text: 'Temperature: ', value: `${source.temp >= 0 ? '+' : ''}${source.temp || 0}` },
-                    { text: 'Feels Like: ', value: `${source.feels_like >= 0 ? '+' : ''}${source.feels_like || 0}` },
-                    { text: 'Humidity: ', value: `${source.humidity || 0}%` },
-                    { text: 'Pressure: ', value: `${source.pressure || 0} hPa` },
-                    { text: 'Wind Speed: ', value: `${source.wind_speed || 0} m/s` },
-                    { text: 'updated at ', value: data.currentWeather.time }
-                ],
-                id: data.id,
-                icon: `http://openweathermap.org/img/w/${data.currentWeather.icon}.png`
-            };
-            setCardInfo(content);
-        }
+        const newCardInfo = createCardData(data);
+        if (newCardInfo) setCardInfo(newCardInfo);
     }, [data]);
 
-    if (!cardInfo) {
-        return <div>loading...</div>
-    }
 
+    if (!cardInfo) return <div>loading...</div>
+    
     return (
         <div className='weather-card'>
             <h3>{cardInfo.header}</h3>
