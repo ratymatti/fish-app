e7b9d3dc05e2263a8170dd8927fde7e227360242
Matti Räty <mattiraty@hotmail.com>
2024-03-06 11:11:02 +0200

    moved transforming and storing card fish data outside from component

--- src/components/FishCard/FishCard.tsx
+++ src/components/FishCard/FishCard.tsx
@@ -1,54 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './FishCard.css';
-import { FishObject } from '../../types/fish';
-
-
-interface CardFish {
-    header: string | null | undefined;
-    info: { text: string; value: string }[];
-    weather: { text: string; value: string | number }[];
-}
+import { CardFish } from '../../types/fish';
 
 interface FishCardProps {
-    userFishArr: FishObject[];
-    currentFishID: string;
+    cardFish: CardFish | null;
     closeCard: () => void;
     handleRemove: (idToRemove: string) => void;
 }
 
-export default function FishCard(props: FishCardProps) {
-    const {
-        closeCard, currentFishID,
-        userFishArr, handleRemove
-    } = props;
-
-    const [cardFish, setCardFish] = useState<CardFish | null>(null);
-
-
-    useEffect(() => {
-        function getFishData() {
-            const fish = userFishArr.find(fish => fish.id === currentFishID);
-            const source = fish?.weather?.currentWeather && 'weather' in fish?.weather?.currentWeather
-                ? fish.weather.currentWeather.weather
-                : undefined;
-            const fishData = {
-                header: fish?.species,
-                info: [
-                    { text: "Length: ", value: `${fish?.cm || 'not available'}${fish?.cm ? 'cm' : ''}` },
-                    { text: "Catch date: ", value: fish?.dateString || 'not available' },
-                    { text: "Catch location: ", value: fish?.locationName || 'not available' }
-                ],
-                weather: [
-                    { text: "Temperature: ", value: `${((source?.temp ? source?.temp : 0) >= 0 ? '+' : '')}${source?.temp ?? "not available"}` },
-                    { text: "Wind direction: ", value: source?.wind_direction || "not available" },
-                    { text: 'Humidity: ', value: `${source?.humidity || "not available"}${source?.humidity ? '%' : ''}` },
-                    { text: 'Pressure: ', value: `${source?.pressure || "not available"}${source?.pressure ? ' hPa' : ''}` },
-                ]
-            }
-            if (fishData) setCardFish(fishData);
-        }
-        getFishData();
-    }, [currentFishID, userFishArr])
+export default function FishCard(props: FishCardProps): JSX.Element | null {
+    const { cardFish, closeCard, handleRemove } = props;
 
     if (cardFish) {
 
@@ -71,12 +32,11 @@ export default function FishCard(props: FishCardProps) {
                         ))
                     }
                     <br></br>
-                    <button onClick={() => handleRemove(currentFishID)}>delete</button>
+                    <button onClick={() => handleRemove(cardFish.id!)}>delete</button>
                 </div>
             </div>
         )
-    } else {
-        return null;
-    }
+    } 
+    return null;
 }
 
