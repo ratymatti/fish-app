c2b040f8fac4589a7db5d78bca343180cb275fd0
Matti Räty <mattiraty@hotmail.com>
2024-03-06 11:12:12 +0200

    changed onClick function

--- src/components/FishRow/FishRow.tsx
+++ src/components/FishRow/FishRow.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { FishObject } from '../../types/fish'
 
+
 interface FishRowProps {
     fish: FishObject;
-    setCurrentFishID: (id: string) => void;
+    handleFishRowClick: (id: string) => void;
 }
 
 export default function FishRow(props: FishRowProps): JSX.Element {
-    const { fish, setCurrentFishID } = props;
-    
+    const { fish, handleFishRowClick } = props;
+
     return (
-        <tr onClick={() => setCurrentFishID(fish.id!)} className='row' key={fish.id}>
+        <tr onClick={() => handleFishRowClick(fish.id!)} className='row' key={fish.id}>
             <td>{fish.species}</td>
             <td>{fish.cm}cm</td>
             <td>{fish.locationName}</td>
