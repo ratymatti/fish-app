121e9a9ce75df48f590138593ff6a514bbe5f265
Matti Räty <mattiraty@hotmail.com>
2024-03-25 18:48:10 +0200

    added state variable userId that stores users id

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -4,16 +4,17 @@ export interface UserContextType {
     isLoggedIn: boolean;
     setIsLoggedIn: (isLoggedIn: boolean) => void;
     userId: string;
+    setUserId: (userId: string) => void;
 }
 
 export const UserContext = React.createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children }: { children: React.ReactNode }): JSX.Element {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const userId = '6a0e1ada-b041-4d80-ab8c-2c69cbdf69a9'; // REMOVE THIS AFTER TESTING
+    const [userId, setUserId] = useState<string>('');
 
     return (
-        <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn, userId }}>
+        <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn, userId, setUserId }}>
             {children}
         </UserContext.Provider>
     )
