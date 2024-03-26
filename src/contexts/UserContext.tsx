f850ec97d832d4c0c980949945806c67464b9f11
Matti Räty <mattiraty@hotmail.com>
2024-03-26 16:10:28 +0200

    removed storing userId

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -3,18 +3,15 @@ import React, { useState } from 'react';
 export interface UserContextType {
     isLoggedIn: boolean;
     setIsLoggedIn: (isLoggedIn: boolean) => void;
-    userId: string;
-    setUserId: (userId: string) => void;
 }
 
 export const UserContext = React.createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children }: { children: React.ReactNode }): JSX.Element {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [userId, setUserId] = useState<string>('');
 
     return (
-        <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn, userId, setUserId }}>
+        <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
             {children}
         </UserContext.Provider>
     )
