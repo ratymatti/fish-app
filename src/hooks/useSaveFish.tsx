bb3d88f1a426e8252bda549de0acf10f34993bf9
Matti Räty <mattiraty@hotmail.com>
2024-03-27 15:05:05 +0200

    created custom hook useSaveFish for saving fish to db

---
+++ src/hooks/useSaveFish.tsx
@@ -0,0 +1,38 @@
+import { NewFishObject } from "../types/fish";
+
+/**
+ * Custom hook for saving fish data
+ * @returns {SaveFish} - object with function for saving fish data
+ */
+
+interface SaveFishDataArgs {
+    idToken: string;
+    newFish: NewFishObject;
+  }
+  
+  interface SaveFish {
+    saveFishData: (args: SaveFishDataArgs) => Promise<any>
+  }
+
+export function useSaveFish(): SaveFish {
+    const urlToFetch = 'http://localhost:8080/fish/save';
+
+    async function saveFishData({ idToken, newFish }: SaveFishDataArgs): Promise<any> {
+        const config: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${idToken}`
+            },
+            body: JSON.stringify(newFish)
+        }
+
+        try {
+            const response = await fetch(urlToFetch, config);
+            console.log(response)
+        } catch (err) {
+            console.error(err);
+        }
+    }
+   return { saveFishData }
+}
