367a828a432a6b1ad1aa78a635e1d46115957bb0
Matti Räty <mattiraty@hotmail.com>
2024-03-27 15:08:26 +0200

    refactored to check that date is also set

--- src/modules/validateForm/validateForm.ts
+++ src/modules/validateForm/validateForm.ts
@@ -4,30 +4,33 @@
  * @description Validates the form by checking if the species, cm, water and location
  *  are provided correctly
  * @param {*} species 
- * @param {*} cm 
+ * @param {*} length 
  * @param {*} locationName 
  * @param {*} fishGeolocation 
  * @returns string or null
  */
 
 interface ValidateFormParams {
-    species: string | null | undefined;
-    cm: number | null | undefined;
-    locationName: string | null | undefined;
+    species: string | null;
+    length: number | null;
+    locationName: string | null;
     geolocation: {
         lat: number;
         lng: number;
-    } | null | undefined;
+    } | null;
+    catchDate: Date | null;
 }
 
-function validateForm({species, cm, locationName, geolocation}: ValidateFormParams): string | null {
+function validateForm({species, length, locationName, geolocation, catchDate}: ValidateFormParams): string | null {
     if (!species) return 'Species is required';
         
-    if (!cm || isNaN(cm) || cm <= 0) return 'Length must be greater than 0';
+    if (!length || isNaN(length) || length <= 0) return 'Length must be greater than 0';
     
     if (!locationName) return 'Location is required.';
     
     if (!geolocation) return 'Geolocation is required.';
+
+    if (!catchDate) return 'Date is required.';
     
     return null;       
 }
