63fbc4bddabcbb64c6adc800917236d118bcdcba
Matti Räty <mattiraty@hotmail.com>
2024-03-29 18:05:20 +0200

    refactored addNewTracking to fetch weather from backend

--- src/contexts/WeatherContext.tsx
+++ src/contexts/WeatherContext.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../config/firebase';
-import { collection, getDocs, /*addDoc,*/ deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { collection, getDocs, /*addDoc,*/ deleteDoc, doc } from 'firebase/firestore';
 import { useFetchWeather } from '../hooks/useFetchWeather';
 import { WeatherObject, WeatherType } from '../types/weather';
 import { LocationContext, LocationContextType } from './LocationContext';
@@ -28,7 +28,7 @@ export function WeatherProvider({ children }: { children: React.ReactNode }): JS
 
     const { getLocation } = React.useContext(LocationContext) as LocationContextType;
 
-    const { fetchWeather, fetchWeatherFromBackend } = useFetchWeather();
+    const { fetchCurrent } = useFetchWeather();
 
     async function getDocuments(): Promise<void> {
         try {
@@ -49,18 +49,11 @@ export function WeatherProvider({ children }: { children: React.ReactNode }): JS
         }
     }
 
-    async function addNewTracking(coords: Location): Promise<void> {
-        await fetchWeatherFromBackend(coords);
-        /*
-        const weatherObject = await fetchWeather(coords, WeatherType.WEATHER);
-        try {
-            await addDoc(weatherRef, weatherObject);
-            getDocuments();
-        } catch (err) {
-            console.error(err);
-        }*/
+    async function addNewTracking(location: Location): Promise<void> {
+        const currentLocationWeather = await fetchCurrent(location);
+        if (currentLocationWeather) setWeatherTrackings([currentLocationWeather]);
     }
-
+    /*
     async function updateWeatherTrackings(): Promise<void> {
         const updatePromises = weatherTrackings.map(async (tracking) => {
             try {
@@ -75,46 +68,14 @@ export function WeatherProvider({ children }: { children: React.ReactNode }): JS
         await Promise.all(updatePromises);
 
         getDocuments();
-    }
+    }*/
 
     async function removeFromTracking(idToRemove: string): Promise<void> {
         const weatherDoc = doc(db, WeatherRef.WEATHER, idToRemove);
         await deleteDoc(weatherDoc);
         getDocuments();
     }
-
-    useEffect(() => {
-        getDocuments();
-    }, []); // Empty dependency array because this needs to run only once for now
-
-    useEffect(() => {
-        const getCurrentWeather = async (): Promise<void> => {
-            try {
-                const location: Location | undefined = await getLocation();
-
-                if (!location) throw new Error('Location is undefined.');
-
-                const currentWeather: WeatherObject | undefined = await fetchWeather(location, WeatherType.WEATHER);
-                
-                if (!currentWeather) throw new Error('Current weather is undefined.');
-
-                setCurrentLocationWeather(currentWeather);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getCurrentWeather();
-    }, []);
     
-    useEffect(() => {
-        const updateInterval = 3600000;
-        let intervalID = setInterval(() => {
-            updateWeatherTrackings();
-        }, updateInterval);
-        return (() => {
-            clearInterval(intervalID);
-        })
-    }, []);
 
     return (
         <WeatherContext.Provider value={{
