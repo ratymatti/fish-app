12e5b18b6dcd313c913baab54e5f53fbe94bc528
Matti Räty <mattiraty@hotmail.com>
2024-04-23 21:45:49 +0300

    created HeaderLogo component

---
+++ src/components/HeaderLogo/HeaderLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeaderLogo() {
+    return (
+        <div className='text-3xl m-4 flex items-center ml-8'>
+            <h1 className='text-white'>Only <span className='bg-orange-400 text-black rounded-md p-1'>Fishes</span></h1>
+        </div>
+    )
+}
