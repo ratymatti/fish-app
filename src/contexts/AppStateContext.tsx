2158b8c2afb5e070fb0281dbfb820fe176b0c172
Matti Räty <mattiraty@hotmail.com>
2024-04-23 12:39:07 +0300

    created AppStateContext to merge UserContext and ActiveContext

---
+++ src/contexts/AppStateContext.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode, createContext, useEffect, useState } from 'react'
+import { useIdToken } from '../hooks/useIdToken';
+
+export enum ActiveState {
+    AddFish = 'add fish',
+    Fishes = 'fishes',
+    Map = 'map',
+    Weather = 'weather',
+    Empty = ''
+}
+
+interface AppStateProps {
+    children: ReactNode;
+}
+
+export interface AppStateContextType {
+    isLoggedIn: boolean;
+    setIsLoggedIn: (isLoggedIn: boolean) => void;
+    active: ActiveState;
+    setActive: (active: ActiveState) => void;
+    error: string;
+    setError: (error: string) => void;
+}
+
+export const AppStateContext = createContext<AppStateContextType | undefined>(undefined);
+
+export function AppStateProvider({ children }: AppStateProps) {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [active, setActive] = useState<ActiveState>(ActiveState.Empty);
+    const [error, setError] = useState<string>('');
+
+    const { initialIdToken } = useIdToken();
+
+    useEffect(() => {
+        if (initialIdToken) {
+            setIsLoggedIn(true);
+        }
+    }, [initialIdToken]);
+
+    return (
+        <AppStateContext.Provider value={{
+            isLoggedIn,
+            setIsLoggedIn,
+            active,
+            setActive,
+            error,
+            setError
+        }}>
+            {children}
+        </AppStateContext.Provider>
+    )
+}
