c01cc73732f2d0dac93cb76b41057816ab1fd323
Matti Räty <mattiraty@hotmail.com>
2024-04-24 21:45:31 +0300

    refactored for using also in Modal

--- src/components/SectionButton/SectionButton.tsx
+++ src/components/SectionButton/SectionButton.tsx
@@ -4,15 +4,18 @@ interface SectionButtonProps {
     children: string;
     onClick: () => void;
     disabled?: boolean;
+    modal?: boolean;
 }
 
-export default function SectionButton({ children, onClick, disabled, ...props }: SectionButtonProps) {
-    let style = 'border uppercase rounded-md text-xs px-2';
+export default function SectionButton({ children, onClick, disabled, modal, ...props }: SectionButtonProps) {
+    let style = 'border uppercase rounded-md px-2';
     
-    if (disabled) {
-        style += ' opacity-30 border-neutral-400 text-neutral-400 bg-neutral-900';
+    if (modal) {
+        style += ' border-orange-400 text-orange-400 hover:text-orange-300 text-sm';
+    } else if (disabled) {
+        style += ' opacity-30 border-neutral-400 text-neutral-400 bg-neutral-900 text-xs';
     } else {
-        style += ' border-orange-400 text-orange-400 hover:text-orange-300';
+        style += ' border-orange-400 text-orange-400 hover:text-orange-300 text-xs';
     }
 
     return (
