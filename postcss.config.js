e0dacdf4870d6591fb0f5ab3fea53b1453692561
Matti Räty <mattiraty@hotmail.com>
2024-04-24 17:29:52 +0300

    importing tailwind to the project

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    plugins: [
+      require('tailwindcss'),
+      require('@tailwindcss/deprecation-warnings'),
+      require('autoprefixer'),
+    ],
+  };
