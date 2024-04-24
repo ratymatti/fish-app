d6c4f6ee0738770298bca93b14a871a4bfeb8c59
Matti Räty <mattiraty@hotmail.com>
2024-04-24 23:27:28 +0300

    renamed MapContainer to MapSection and created new MapContainer component that actually is container xd

--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -1,40 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Map from '../Map/Map';
-import { FishContext, FishContextType } from '../../contexts/FishContext';
-import { Location, LocationObject } from '../../types/location';
-import { FishObject } from '../../types/fish'; 
-import { AppStateContext, AppStateContextType } from '../../contexts/AppStateContext';
+import React, { ReactNode } from 'react'
 
-const filterAndTransformFishes = (userFishArr: FishObject[]): LocationObject[] => {
-    return userFishArr
-        .filter(fish => fish.id && fish.geolocation)
-        .map(fish => ({
-            id: fish.id as string,
-            geolocation: {
-                lat: (fish.geolocation as Location).lat,
-                lng: (fish.geolocation as Location).lng
-            }
-        }));
+interface MapContainerProps {
+    children: ReactNode;
 }
 
-export default function MapContainer(): JSX.Element {
-    const { userFishArr } = useContext(FishContext) as FishContextType;
-    const { userLocation } = useContext(AppStateContext) as AppStateContextType;
-
-    const [fishGeolocationArr, setFishGeolocationArr] = useState<LocationObject[]>([]);
-
-    useEffect(() => {
-        const validFishArray: LocationObject[] = filterAndTransformFishes(userFishArr);
-        setFishGeolocationArr(validFishArray);
-    }, [userFishArr]);
-
+export default function MapContainer({ children }: MapContainerProps) {
     return (
-        <div className='h-1/2 w-full mx-8'>
-            <Map
-                center={userLocation}
-                zoom={10}
-                markerLocations={fishGeolocationArr} />
+        <div className='relative h-1/2 w-full mx-8'>
+            {children}
         </div>
     )
 }
-
---
+++ src/components/MapSection/MapSection.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Map from '../Map/Map';
+import { FishContext, FishContextType } from '../../contexts/FishContext';
+import { Location, LocationObject } from '../../types/location';
+import { FishObject } from '../../types/fish';
+import { AppStateContext, AppStateContextType } from '../../contexts/AppStateContext';
+import MapContainer from '../MapContainer/MapContainer';
+
+const filterAndTransformFishes = (userFishArr: FishObject[]): LocationObject[] => {
+    return userFishArr
+        .filter(fish => fish.id && fish.geolocation)
+        .map(fish => ({
+            id: fish.id as string,
+            geolocation: {
+                lat: (fish.geolocation as Location).lat,
+                lng: (fish.geolocation as Location).lng
+            }
+        }));
+}
+
+export default function MapSection(): JSX.Element {
+    const { userFishArr } = useContext(FishContext) as FishContextType;
+    const { userLocation } = useContext(AppStateContext) as AppStateContextType;
+
+    const [fishGeolocationArr, setFishGeolocationArr] = useState<LocationObject[]>([]);
+
+    useEffect(() => {
+        const validFishArray: LocationObject[] = filterAndTransformFishes(userFishArr);
+        setFishGeolocationArr(validFishArray);
+    }, [userFishArr]);
+
+    return (
+        <MapContainer>
+            <Map
+                center={userLocation}
+                zoom={10}
+                markerLocations={fishGeolocationArr} />
+        </MapContainer>
+    )
+}
+
