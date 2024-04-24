7ed6edd5887c30d0f2e6b04d71d53a11947aae2b
Matti Räty <mattiraty@hotmail.com>
2024-04-24 21:45:01 +0300

    added Modal for requesting permission to get users geolocation

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import AddContainer from '../AddContainer/AddContainer';
 import Log from '../Log/Log';
 import MapContainer from '../MapContainer/MapContainer';
@@ -6,15 +6,39 @@ import Weather from '../Weather/Weather';
 import Auth from '../Auth/Auth';
 import { CreateFishProvider } from '../../contexts/CreateFishContext';
 import { AppStateContext, AppStateContextType, ActiveState } from '../../contexts/AppStateContext';
+import Modal from '../Modal/Modal';
+import { useModal } from '../../hooks/useModal';
+import LocationModal from '../LocationModal/LocationModal';
 
 interface MainContainerProps {
     setFreeze: (freeze: boolean) => void;
 }
 
 export default function MainContainer({ setFreeze }: MainContainerProps) {
-    const { isLoggedIn, active } = useContext(AppStateContext) as AppStateContextType;
+    const { isLoggedIn, active, userLocation, getAndSetLocation } = useContext(AppStateContext) as AppStateContextType;
+
+    const { modalRef, openModal, closeModal } = useModal();
+
+    function handleAllow() {
+        getAndSetLocation();
+        closeModal();
+    }
+
+    function handleDeny() {
+        closeModal();
+    }
+
+    useEffect(() => {
+        if (isLoggedIn && !userLocation) {
+            openModal();
+        } 
+    }, [isLoggedIn, userLocation]);
 
     return (
+        <>
+            <Modal ref={modalRef}>
+                <LocationModal onAllow={handleAllow} onDeny={handleDeny} />
+            </Modal>
             <div className='h-screen px-8 mt-8 flex justify-center'>
                 {active === ActiveState.AddFish &&
                     <CreateFishProvider>
@@ -30,6 +54,7 @@ export default function MainContainer({ setFreeze }: MainContainerProps) {
                 {!isLoggedIn &&
                     <Auth />}
             </div>
+        </>
     )
 }
 
