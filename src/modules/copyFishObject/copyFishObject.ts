48cedbc9bf75f6fbd33a91a4cc2a5bb0cf6cc54c
Matti Räty <mattiraty@hotmail.com>
2024-04-25 14:40:41 +0300

    created copyFishObject function

---
+++ src/modules/copyFishObject/copyFishObject.ts
@@ -0,0 +1,26 @@
+import { NewFishObject, RequestFishObject } from "../../types/fish";
+
+/**
+ * Values can be type asserted to be non-null here because
+ * this function is only called when those are validated.
+ *  
+ * @param newFishData as NewFishObject
+ * @returns RequestFishObject
+ */
+
+function copyFishObject(newFishData: NewFishObject): RequestFishObject {
+    const currentFishData: RequestFishObject = {
+        species: newFishData.species!,
+        length: newFishData.length!,
+        date: newFishData.date!.toISOString(),
+        locationName: newFishData.locationName!,
+        comment: newFishData.comment,
+        geolocation: {
+            lat: newFishData.geolocation.lat,
+            lng: newFishData.geolocation.lng,
+        }
+    };
+    return currentFishData;
+}
+
+export default copyFishObject;
