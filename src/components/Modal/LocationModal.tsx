96be90c6f304d88e3fe90fb4d614f7c419248591
Matti Räty <mattiraty@hotmail.com>
2024-04-27 14:02:54 +0300

    small changes to styling

--- src/components/Modal/LocationModal.tsx
+++ src/components/Modal/LocationModal.tsx
@@ -9,10 +9,10 @@ interface LocationModalProps {
 export default function LocationModal({ onAllow, onDeny }: LocationModalProps) {
     return (
         
-            <div className='border border-neutral-800 h-full w-full flex flex-col justify-between items-center py-8 bg-neutral-700'>
+            <div className='border border-neutral-800 h-full w-full flex flex-col justify-between items-center py-6 bg-neutral-700'>
                 <h3 className='uppercase text-sm mt-2 text-neutral-300'>App want's to use your location</h3>
                 <p className='mx-4 my-6 text-center text-xs text-neutral-400'>Location information is used only for providing weather data.</p>
-                <div className='flex w-1/2 justify-around my-6 px-2'>
+                <div className='flex w-1/2 justify-around px-2'>
                     <SectionButton modal onClick={onAllow}>Allow</SectionButton>
                     <SectionButton modal onClick={onDeny}>Deny</SectionButton>
                 </div>
