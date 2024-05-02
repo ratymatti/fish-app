56c0e802b320cad8800523126203d24ded2bc66c
Matti Räty <mattiraty@hotmail.com>
2024-05-02 22:11:37 +0300

    created functionality for user to be able to center the map to their current location

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,13 +1,26 @@
-import React, { ReactNode } from 'react'
+import React, { ReactElement, useContext, useState } from 'react'
+import SectionButton from '../Main/SectionButton';
+import { AppStateContext, AppStateContextType } from '../../contexts/AppStateContext';
 
 interface MapContainerProps {
-    children: ReactNode;
+    children: ReactElement;
 }
 
 export default function MapContainer({ children }: MapContainerProps) {
+    const { mapRef, userLocation } = useContext(AppStateContext) as AppStateContextType;
+    const [key, setKey] = useState<number>(Math.random()); // Used for forcing update of the map
+
+    function handleCenter() {
+       if (userLocation) mapRef.current = userLocation;
+       setKey(Math.random()); // Force update of the map
+    }
+
     return (
         <div className='relative h-1/2 w-full mx-8'>
-            {children}
+            <div className='flex justify-end mb-2'>
+                <SectionButton onClick={handleCenter}>center to current location</SectionButton>
+            </div>
+            {React.cloneElement(children, { key })}
         </div>
     )
 }
